3d64025fda1f5e12ea5d6708ab680c16c17ab997
Connor Turland <connorturland@gmail.com>
2021-07-12 10:26:40 -0700

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Sprillow`,
-    description: `Sprillow Limited is a design, development and consultation studio focused on co-creating systems change initiatives. It was founded in July of 2019.`,
+    description: `Sprillow Limited is a design & development studio specializing in distributed systems software applications. It was founded in July of 2019.`,
     author: `@Sprillow`,
     siteUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://sprillow.com'
   },
@@ -56,6 +56,46 @@ module.exports = {
         theme_color: `#1B2723`,
         display: `minimal-ui`,
         icon: `src/images/sprillow-logo-with-text-anzac.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-amplitude-analytics`,
+      options: {
+        // Specify the API key for your Amplitude Project (required)
+        apiKey: "70b44a7909b7b50b16441d99fe68f24d",
+        // Puts tracking script in the head instead of the body (optional)
+        head: false,
+        // Prevents loading Amplitude and logging events if visitors have "Do Not Track" enabled (optional)
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths (optional)
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Override the default event types (optional)
+        // eventTypes: {
+        //   outboundLinkClick: 'OUTBOUND_LINK_CLICK',
+        //   pageView: 'PAGE_VIEW',
+        // },
+        // Amplitude JS SDK configuration options (optional)
+        amplitudeConfig: {
+          // no cookies!
+          storage: 'localStorage',
+          trackingOptions: {
+            city: true,
+            country: true,
+            carrier: false,
+            device_manufacturer: true,
+            device_model: true,
+            dma: true,
+            ip_address: false,
+            language: true,
+            os_name: true,
+            os_version: true,
+            platform: true,
+            region: true,
+            version_name: true,
+          }
+        },
+        // Specify NODE_ENVs in which the plugin should be loaded (optional)
+        environments: ["production"],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
